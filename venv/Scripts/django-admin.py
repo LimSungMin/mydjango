9af6ae1e7fbf5351db4c1b9413640a3f256cd67c
임성민 <lsmorigin@gmail.com>
2018-07-30 11:54:23 +0900

    add blog model & template

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\user\Documents\git\mydjango\venv\Scripts\python.exe
+#!c:\users\user\documents\git\mydjango\venv\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
